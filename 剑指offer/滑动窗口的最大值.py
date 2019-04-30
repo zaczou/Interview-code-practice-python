61ced253c9cf5ee8c150f3534c284317268b7b24
zouyang <714232765@qq.com>
2019-04-30 15:33:29 +0800

    Update 滑动窗口的最大值.py

--- 剑指offer/滑动窗口的最大值.py
+++ 剑指offer/滑动窗口的最大值.py
@@ -43,4 +43,26 @@ class Solution:
 
 
 '''
-
+'''
+# -*- coding:utf-8 -*-
+class Solution:
+    def maxInWindows(self, num, size):
+        # write code here
+        # 存放可能是最大值的下标
+        maxqueue = []
+        # 存放窗口中最大值
+        maxlist = []
+        n = len(num)
+        # 参数检验
+        if n == 0 or size == 0 or size > n:
+            return maxlist
+        for i in range(n):
+            # 判断队首下标对应的元素是否已经滑出窗口
+            if len(maxqueue) > 0 and i - size >= maxqueue[0]:
+                maxqueue.pop(0)
+            while len(maxqueue) > 0 and num[i] > num[maxqueue[-1]]:
+                maxqueue.pop()
+            maxqueue.append(i)
+            if i >= size - 1:
+                maxlist.append(num[maxqueue[0]])
+        return maxlist
