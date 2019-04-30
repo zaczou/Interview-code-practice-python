ffa55a2e5ccf1327bceab29756c1366750982dd4
zouyang <714232765@qq.com>
2019-04-30 14:23:22 +0800

    Update 二叉树中和为某一值的路径.py
    增加其他人解法

--- 剑指offer/二叉树中和为某一值的路径.py
+++ 剑指offer/二叉树中和为某一值的路径.py
@@ -50,3 +50,25 @@ class Solution:
 
         FindPath2(root, [], 0)
         return result
+
+    # -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPath(self, root, expectNumber):
+        # write code here
+        if not root:
+            return []
+        #	如果只有根节点或者找到叶子节点，我们就把其值返回
+        if root and not root.left and not root.right and root.val==expectNumber:
+            return [[root.val]]
+        res = []
+        left = self.FindPath(root.left,expectNumber-root.val)
+        right = self.FindPath(root.right,expectNumber-root.val)
+        for i in left+right:
+            res.append([root.val]+i)
+        return res
